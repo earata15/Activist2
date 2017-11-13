7e6f294d0776f72a21191d317da98aae5509d148
Emma <earata11@gmail.com>
2017-11-12 21:28:01 -0500

    pulls info from one search page (search term 'activist') and prints results into console

--- testscrape.js
+++ testscrape.js
@@ -13,7 +13,7 @@ var cheerio = require("cheerio");
 var app = express();
 
 
-url = "https://www.gofundme.com";
+url = "https://www.gofundme.com/mvc.php?route=category&term=activist";
 
 	//the structure of our request call
 	// the first parameter is our URL
@@ -26,17 +26,40 @@ var options = {
 request(url, options, function(error, response, html){
 	
 	if(!error){
-		// Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+		// utilize the cheerio library on the returned html which will essentially give us jQuery functionality
 		var $ = cheerio.load(html);
-		console.log(html);
-		console.log("logged html");
 
-		// this would be the beginnings of an attempt to isolate some links w/ titles
-		// didn't get to test because of 403 error from GoFundMe
-		$('div.tile-title').each(function(i, element){
-	        var a = $(this).prev();
-     			console.log(a.text());
-     			console.log("logged");
-	    });
+		// pull search results: titles, URLs, description
+		// search results can be found under the class name js-fund-tile
+		//filter by this class, find specific info from there	
+		$('.js-fund-tile').each(function(i,element){
+			// unsure if this syntax is ugly or not but it seems to work fine
+			// I don't think I can use a different variable name besides '$'
+			var $ = cheerio.load(element);
+
+			// pull title info
+			$('div .tile-title').each(function(j, elt){
+				var data = $(this);
+				console.log("\n\nResult " + i +":\nTitle: "+ data.text());
+			});
+
+			//pull URL for search result
+			console.log("URL: " + $('a', 'div .js-tile-img-contain').attr('href'));
+			
+			// pull description 
+			$('div .tile-footer').each(function(j, elt){
+				var data = $(this);
+				console.log("Description: "+data.text());
+			});
+		});	
+/*
+		$('div .tile-title').each(function(i, element){
+			var data = $(this);
+
+	        console.log(data.text());
+	        console.log("logged");
+	        			
+	    });*/
+
 	}
 });
